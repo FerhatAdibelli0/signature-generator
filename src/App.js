391c2bf1428edfa6653640adc665338183df2763
Maxpayne <ferhatadibelli9@gmail.com>
2022-03-14 14:15:31 +0300

    initiation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Card from "./UI/Card";
+
+function App() {
+  return (
+    <div>
+      <Card>
+        <h2>This is header one</h2>
+      </Card>
+      <Card>
+        <h2>This is header two</h2>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
